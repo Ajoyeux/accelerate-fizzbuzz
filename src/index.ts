91a94a49dc652bd9baf65897f35dfaed1a48c214
occitaneUbald <Ubald.DEBELLABRE.ext@loccitane.com>
2023-09-14 09:53:56 +0200

    Add appInsights

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { init } from './server.js';
 import { Logger } from './logger.js';
 import { createHelloWorldRoute } from './routes/get-hello-world.js';
+import dotenv from 'dotenv';
+import process from 'process';
+import appInsights from 'applicationinsights';
+
+dotenv.config();
+appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start();
+appInsights.defaultClient.context.tags['ai.cloud.role'] = 'NAME-ME'; // TODO Use application name instead of NAME-ME
 
 const logger: Logger = {
-    logError: (exception) => console.log(exception),
+    logError: (exception) => {
+        if (exception instanceof Error) appInsights.defaultClient.trackException({ exception });
+    },
 };
 
 const server = await init({ logger });
